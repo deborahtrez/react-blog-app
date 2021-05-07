56bb5acc0a9a098812b138a316343c27e652922a
deborahtrez <deborahtrez12@gmail.com>
2021-05-07 03:59:33 +0300

    Updates

---
+++ precache-manifest.b12af16d810170da3208dbe4caef90b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe6eb2ce361e6539600d1ea5598c321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531323e63e589e2e8507",
+    "url": "/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "d8052f0f9f414ed2cbb0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "531323e63e589e2e8507",
+    "url": "/static/js/2.26249e07.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.26249e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8052f0f9f414ed2cbb0",
+    "url": "/static/js/main.8a4cdc07.chunk.js"
+  },
+  {
+    "revision": "98ec7aaa60c4af0ab39f",
+    "url": "/static/js/runtime-main.d36339e0.js"
+  }
+]);
